320eb5fe4882bc9fc15de555486ca6dda8ddfb12
Azaz Akhtar <azaz.akhtar@powerschool.com>
2018-08-26 21:53:53 +0530

    786/new build

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export * from './src/abstract-server';
-export { EZServerConfig } from './src/models/server-config';
+export { EZISwaggerConfig } from './src/models/ezi-swagger-config';
 export * from './src/db/connection-pool';
 export * from './src/db/db-util';
 export * from './src/models/ezi-request';
